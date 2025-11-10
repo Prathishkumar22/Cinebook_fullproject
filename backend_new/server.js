9ded75ec97049371c21c073c527104299e8b5c55
Prathishkumar22 <167306834+Prathishkumar22@users.noreply.github.com>
2025-11-10 11:09:02 +0530

    CD added

--- backend_new/server.js
+++ backend_new/server.js
@@ -20,7 +20,7 @@ app.use(cors({
 // });
 
 app.use(express.json());
-app.use('/api/users', routes);
+app.use('/api', routes);
 app.listen(PORT, async () => {
   await connectDB();
   console.log(`Server running on port ${PORT}`);
