90dd145500b9c69c2779e0429d760fd6d046aa11
Prathishkumar22 <167306834+Prathishkumar22@users.noreply.github.com>
2025-11-10 14:40:20 +0530

    CD added

--- Cinebook/src/pages/Home.jsx
+++ Cinebook/src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom'
-import logo from '../assets/Cinebook-logo.png'
+import logo from '../assets/Cinebook-logo-CH6NRZlE.png'
 import styles from '../pages/Home.module.css'
 import { useEffect, useState } from 'react'
 
